41e3bdd07ad3cbc62f2fdc168e6ac38567704d5d
Rohit Bhatt <rbhatt199924@gmail.com>
2023-09-21 11:50:19 +0530

    add tag manager

--- src/components/GoogleAnalytic.jsx
+++ src/components/GoogleAnalytic.jsx
@@ -1,12 +1,18 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import Script from 'next/script';
+import TagManager from 'react-gtm-module';
 
 export default function GoogleAnalytic() {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-PCN84MFT' });
+}, []);
   return (
     <>
       <GoogleAnalytics />
+
       <Script
         id="adsbygoogle-init"
         strategy="afterInteractive"
