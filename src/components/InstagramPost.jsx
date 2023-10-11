a4ba1fd9599d9006de156fbe6d949ecd08510648
Rohit Bhatt <rbhatt199924@gmail.com>
2023-10-11 22:03:47 +0530

    update instagram

--- src/components/InstagramPost.jsx
+++ src/components/InstagramPost.jsx
@@ -1,14 +1,25 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { InstagramEmbed } from 'react-social-media-embed';
-import { a } from 'react-spring';
 
 export default function InstagramPost({ url }) {
+  const [mobile, setMobile] = React.useState(false)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if(window.innerWidth < 768) {
+        setMobile(true)
+      }
+      else {
+        setMobile(false)
+      }
+    }
+  }, [])
   return (
     <>
       <InstagramEmbed url={url} style={{
         maxWidth: 550
-      }} width="100%" />
+      }} width={mobile ? 320 : 550} />
     </>
   )
 }
