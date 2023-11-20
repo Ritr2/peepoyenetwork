d7a307d5ebaf3e3088d16be754eb21ef4f43108b
Rohit Bhatt <rohit.bhatt@your-space.in>
2023-11-20 21:41:00 +0530

    update page

--- src/app/(productsLandingPage)/(money-webinar)/mewebinar1/page.jsx
+++ src/app/(productsLandingPage)/(money-webinar)/mewebinar1/page.jsx
@@ -129,7 +129,7 @@ export default function page() {
   return (
     <main className={`flex flex-col items-center justify-center w-full ${dmSans.className}`}>
       <section className='flex flex-col items-center justify-center w-full webinar-background'>
-        <div className='flex flex-col items-center justify-center bg-black/70 gap-5 md:gap-10 w-full flex-1 px-5 py-10 md:px-40'>
+        <div className='flex flex-col items-center justify-center bg-black/70 gap-5 md:gap-10 w-full flex-1 px-5 py-10 md:px-24'>
           <div className='flex flex-col items-center justify-center w-full gap-2 md:gap-4 flex-1'>
             <h1 className='text-xl md:text-6xl font-bold text-center text-white md:leading-[55px]'>Learn How To Build Your <span className='red-text'>Own Money Engine</span> Using Youtube Within 3 Days</h1>
             <p className='text-lg md:text-2xl font-normal text-center text-white'>
@@ -138,13 +138,13 @@ export default function page() {
           </div>
           <div className='flex flex-col md:flex-row justify-center gap-5 md:gap-10 w-full'>
             <div className='flex flex-col w-full md:w-6/12'>
-              <div className='relative flex-1 w-full h-auto overflow-hidden order-2 md:order-1 min-h-[160px] md:min-h-[360px]' style={{ position: 'relative' }}>
+              <div className='relative flex-1 w-full h-auto overflow-hidden order-2 md:order-1 min-h-[160px] md:min-h-[345px]' style={{ position: 'relative' }}>
                 <iframe src="https://www.loom.com/embed/def564694cdb4d48a337b6190fd8c7d7?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true&showEmbedControls=false&show_date=false&loop=1"
                   frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', borderRadius: "10px" }} autoPlay
                 ></iframe>
               </div>
             </div>
-            <div className="relative flex flex-col items-center justify-center flex-1 p-3 md:p-5 gap-1 md:gap-2 rounded-lg bg-neutral-100 border-[4px] border-red-600 border-dashed">
+            <div className="relative flex flex-col items-center justify-center flex-1 p-1 md:p-3 gap-1 md:gap-2 rounded-lg bg-neutral-100 border-[4px] border-red-600 border-dashed">
               <span className='text-xl md:text-2xl font-bold text-center'>Date <span className='red-text'>|</span> Saturday <span className='red-text'>|</span> 9PM to 10PM IST</span>
               <Form text="Signup for LIVE Webinar" />
               <span className="text-base md:text-xl text-center text-neutral-500 font-bold"><span className='font-black red-text text-4xl'>+</span><br />Get my Influencer Guide on how to Become a Top Influencer Fast totally Free with this webinar</span>
