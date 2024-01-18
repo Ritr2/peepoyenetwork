4028be3a16be1fd9dd99c2870104811d49116ef9
rohitpeepoye <138089975+rohitpeepoye@users.noreply.github.com>
2024-01-18 19:46:10 +0530

    Update scores.js

--- src/utils/leaderboard/scores.js
+++ src/utils/leaderboard/scores.js
@@ -1,160 +1,159 @@
 const weeklyScores = [
-    {
-        "rank": 1,
-        "name": "Sanjeet Kumar",
-        "score": 21
-    },
-    {
-        "rank": 2,
-        "name": "Sahil Khan",
-        "score": 20
-    },
-    {
-        "rank": 3,
-        "name": "Sanidhay Grover",
-        "score": 16
-    },
-    {
-        "rank": 4,
-        "name": "Shivangi Jajoo",
-        "score": 10
-    },
-    {
-        "rank": 5,
-        "name": "Akarsh Pratap Singh",
-        "score": 9
-    },
-    {
-        "rank": 6,
-        "name": "Kunal Priyadarshi",
-        "score": 6
-    },
-    {
-        "rank": 7,
-        "name": "Tanmoy Singha",
-        "score": 6
-    },
-    {
-        "rank": 8,
-        "name": "Akarshit Mehta",
-        "score": 6
-    },
-    {
-        "rank": 9,
-        "name": "Himanshu  Pandey",
-        "score": 5
-    },
-    {
-        "rank": 10,
-        "name": "Shashank Selvarajan Pillay",
-        "score": 4
-    }
+  {
+    "rank": 1,
+    "name": "Sahil Khan",
+    "score": 22
+  },
+  {
+    "rank": 2,
+    "name": "Sanjeet Kumar",
+    "score": 21
+  },
+  {
+    "rank": 3,
+    "name": "Sanidhay Grover",
+    "score": 16
+  },
+  {
+    "rank": 4,
+    "name": "Shivangi Jajoo",
+    "score": 10
+  },
+  {
+    "rank": 5,
+    "name": "Akarsh Pratap Singh",
+    "score": 9
+  },
+  {
+    "rank": 6,
+    "name": "Kunal Priyadarshi",
+    "score": 6
+  },
+  {
+    "rank": 7,
+    "name": "Tanmoy Singha",
+    "score": 6
+  },
+  {
+    "rank": 8,
+    "name": "Akarshit Mehta",
+    "score": 6
+  },
+  {
+    "rank": 9,
+    "name": "Himanshu  Pandey",
+    "score": 5
+  },
+  {
+    "rank": 10,
+    "name": "Shashank Selvarajan Pillay",
+    "score": 4
+  }
 ]
 
 const monthlyScores = [
-    {
-        "rank": 1,
-        "name": "Sanjeet Kumar",
-        "score": 21
-    },
-    {
-        "rank": 2,
-        "name": "Sahil Khan",
-        "score": 20
-    },
-    {
-        "rank": 3,
-        "name": "Sanidhay Grover",
-        "score": 16
-    },
-    {
-        "rank": 4,
-        "name": "Shivangi Jajoo",
-        "score": 10
-    },
-    {
-        "rank": 5,
-        "name": "Akarsh Pratap Singh",
-        "score": 9
-    },
-    {
-        "rank": 6,
-        "name": "Kunal Priyadarshi",
-        "score": 6
-    },
-    {
-        "rank": 7,
-        "name": "Tanmoy Singha",
-        "score": 6
-    },
-    {
-        "rank": 8,
-        "name": "Akarshit Mehta",
-        "score": 6
-    },
-    {
-        "rank": 9,
-        "name": "Himanshu  Pandey",
-        "score": 5
-    },
-    {
-        "rank": 10,
-        "name": "Shashank Selvarajan Pillay",
-        "score": 4
-    }
+  {
+    "rank": 1,
+    "name": "Sahil Khan",
+    "score": 22
+  },
+  {
+    "rank": 2,
+    "name": "Sanjeet Kumar",
+    "score": 21
+  },
+  {
+    "rank": 3,
+    "name": "Sanidhay Grover",
+    "score": 16
+  },
+  {
+    "rank": 4,
+    "name": "Shivangi Jajoo",
+    "score": 10
+  },
+  {
+    "rank": 5,
+    "name": "Akarsh Pratap Singh",
+    "score": 9
+  },
+  {
+    "rank": 6,
+    "name": "Kunal Priyadarshi",
+    "score": 6
+  },
+  {
+    "rank": 7,
+    "name": "Tanmoy Singha",
+    "score": 6
+  },
+  {
+    "rank": 8,
+    "name": "Akarshit Mehta",
+    "score": 6
+  },
+  {
+    "rank": 9,
+    "name": "Himanshu  Pandey",
+    "score": 5
+  },
+  {
+    "rank": 10,
+    "name": "Shashank Selvarajan Pillay",
+    "score": 4
+  }
 ]
 
 const allTimeScores = [
-    {
-        "rank": 1,
-        "name": "Sanjeet Kumar",
-        "score": 21
-    },
-    {
-        "rank": 2,
-        "name": "Sahil Khan",
-        "score": 20
-    },
-    {
-        "rank": 3,
-        "name": "Sanidhay Grover",
-        "score": 16
-    },
-    {
-        "rank": 4,
-        "name": "Shivangi Jajoo",
-        "score": 10
-    },
-    {
-        "rank": 5,
-        "name": "Akarsh Pratap Singh",
-        "score": 9
-    },
-    {
-        "rank": 6,
-        "name": "Kunal Priyadarshi",
-        "score": 6
-    },
-    {
-        "rank": 7,
-        "name": "Tanmoy Singha",
-        "score": 6
-    },
-    {
-        "rank": 8,
-        "name": "Akarshit Mehta",
-        "score": 6
-    },
-    {
-        "rank": 9,
-        "name": "Himanshu  Pandey",
-        "score": 5
-    },
-    {
-        "rank": 10,
-        "name": "Shashank Selvarajan Pillay",
-        "score": 4
-    }
+  {
+    "rank": 1,
+    "name": "Sahil Khan",
+    "score": 22
+  },
+  {
+    "rank": 2,
+    "name": "Sahil Khan",
+    "score": 20
+  },
+  {
+    "rank": 3,
+    "name": "Sanidhay Grover",
+    "score": 16
+  },
+  {
+    "rank": 4,
+    "name": "Shivangi Jajoo",
+    "score": 10
+  },
+  {
+    "rank": 5,
+    "name": "Akarsh Pratap Singh",
+    "score": 9
+  },
+  {
+    "rank": 6,
+    "name": "Kunal Priyadarshi",
+    "score": 6
+  },
+  {
+    "rank": 7,
+    "name": "Tanmoy Singha",
+    "score": 6
+  },
+  {
+    "rank": 8,
+    "name": "Akarshit Mehta",
+    "score": 6
+  },
+  {
+    "rank": 9,
+    "name": "Himanshu  Pandey",
+    "score": 5
+  },
+  {
+    "rank": 10,
+    "name": "Shashank Selvarajan Pillay",
+    "score": 4
+  }
 ]
-
 export { weeklyScores, monthlyScores, allTimeScores }
